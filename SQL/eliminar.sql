f5be388410da50b7b6340bb2241b68f3d676157e
Erick Abarca Calderón <erimanuabca@gmail.com>
2024-09-30 00:12:26 -0600

    Creado el SP de eliminar, funciona correctamente.

---
+++ SQL/eliminar.sql
@@ -0,0 +1,90 @@
+ALTER PROCEDURE [dbo].[IntentoBorrado]
+    @ValorDocumentoIdentidad VARCHAR(128),
+    @Username VARCHAR(128),
+    @PostInIP VARCHAR(64),
+    @Confirmación INT,
+    @OutResultCode INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        DECLARE @NombreEmpleado VARCHAR(128);
+        DECLARE @PuestoEmpleado VARCHAR(128);
+        DECLARE @SaldoVacaciones INT;
+
+
+        IF EXISTS (SELECT 1 FROM dbo.Empleado WHERE ValorDocumentoIdentidad = @ValorDocumentoIdentidad)
+
+        SELECT 
+                @NombreEmpleado = E.Nombre,
+                @PuestoEmpleado = P.Nombre,
+                @SaldoVacaciones = E.SaldoVacaciones
+        FROM dbo.Empleado E
+        INNER JOIN dbo.Puesto P ON E.IdPuesto = P.Id
+        WHERE E.ValorDocumentoIdentidad = @ValorDocumentoIdentidad;        
+
+        BEGIN
+            IF @Confirmación = 0
+                BEGIN
+                    INSERT INTO dbo.bitacoraEvento(
+                        IdTipoEvento,
+                        IdUsuario,
+                        Fecha,
+                        Descripcion,
+                        PostInIP,
+                        PostTime
+                    ) VALUES (
+                        8,
+                        (SELECT Id FROM dbo.Usuario WHERE Username = @Username),
+                        GETDATE(),
+                        CONCAT('Intento de borrado: [ValorDocumento: ', @ValorDocumentoIdentidad,                             ', Nombre: ', @NombreEmpleado, 
+                        ', Puesto: ', @PuestoEmpleado, 
+                        ', SaldoVacaciones: ', @SaldoVacaciones, ']'),
+                        @PostInIP,
+                        GETDATE()
+                    );
+                END
+        ELSE
+            BEGIN
+                UPDATE dbo.Empleado 
+                SET EsActivo = 0 
+                WHERE ValorDocumentoIdentidad = @ValorDocumentoIdentidad;
+                SET @OutResultCode = 0;
+
+
+                INSERT INTO dbo.bitacoraEvento(
+                    IdTipoEvento,
+                    IdUsuario,
+                    Fecha,
+                    Descripcion,
+                    PostInIP,
+                    PostTime
+                ) VALUES (
+                    9,
+                    (SELECT Id FROM dbo.Usuario WHERE Username = @Username),
+                    GETDATE(),
+                    CONCAT('Borrado: [ValorDocumento: ', @ValorDocumentoIdentidad, 
+                    ', Nombre: ', @NombreEmpleado, 
+                    ', Puesto: ', @PuestoEmpleado, 
+                    ', SaldoVacaciones: ', @SaldoVacaciones, ']'),
+                    @PostInIP,
+                    GETDATE()
+                );
+            END
+        END 
+    END TRY
+    BEGIN CATCH
+    INSERT INTO dbo.DBErrors VALUES (
+            SUSER_SNAME(),
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE(),
+            GETDATE()
+        );
+    END CATCH
+    SET NOCOUNT OFF;
+END
+GO
--- SQL/listar.sql
+++ SQL/listar.sql
@@ -1,39 +1,39 @@
 ALTER PROCEDURE [dbo].[ListarEmpleado]
-	@OutResulTCode INT OUTPUT
+    @OutResulTCode INT OUTPUT
 AS
 BEGIN
-	SET NOCOUNT ON;
-	BEGIN TRY
-	
-	SET @OutResulTCode=0;
+    SET NOCOUNT ON;
+    BEGIN TRY
+    
+        SET @OutResulTCode = 0;
 
-SELECT E.[Id]  
-       , E.[ValorDocumentoIdentidad]
-       , E.[Nombre]
-	   , P.[Nombre] AS Puesto
-       , E.[FechaContratacion]
-       , E.[SaldoVacaciones]
-       , E.[EsActivo] 
-FROM dbo.Empleado E
-INNER JOIN dbo.Puesto P ON E.IdPuesto = P.Id
-WHERE E.EsActivo = 1
-ORDER BY E.Nombre;
+        SELECT E.[Id]  
+               , E.[ValorDocumentoIdentidad]
+               , E.[Nombre]
+               , P.[Nombre] AS Puesto
+               , E.[FechaContratacion]
+               , E.[SaldoVacaciones]
+               , E.[EsActivo] 
+        FROM dbo.Empleado E
+        INNER JOIN dbo.Puesto P ON E.IdPuesto = P.Id
+        WHERE E.EsActivo = 1 
+        ORDER BY E.Nombre;
 
-	END TRY
-	BEGIN CATCH
-		INSERT INTO dbo.DBErrors	VALUES (
-			SUSER_SNAME(),
-			ERROR_NUMBER(),
-			ERROR_STATE(),
-			ERROR_SEVERITY(),
-			ERROR_LINE(),
-			ERROR_PROCEDURE(),
-			ERROR_MESSAGE(),
-			GETDATE()
-		);
+    END TRY
+    BEGIN CATCH
+        INSERT INTO dbo.DBErrors VALUES (
+            SUSER_SNAME(),
+            ERROR_NUMBER(),
+            ERROR_STATE(),
+            ERROR_SEVERITY(),
+            ERROR_LINE(),
+            ERROR_PROCEDURE(),
+            ERROR_MESSAGE(),
+            GETDATE()
+        );
 
-		SET @OutResulTCode=50008; 
-	END CATCH;
+        SET @OutResulTCode = 50008; 
+    END CATCH;
 
-	SET NOCOUNT Off;
+    SET NOCOUNT OFF;
 END;
